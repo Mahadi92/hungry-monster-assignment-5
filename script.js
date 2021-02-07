a438b247abecd39b13e2a01447678fc5e8652bdd
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2021-02-07 14:16:22 +0600

    Search and Show meals COMPATED

--- script.js
+++ script.js
@@ -5,15 +5,32 @@ document.getElementById('search').addEventListener('click', function () {
         .then(res => res.json())
         .then(data => showMeal(data.meals))
 
-//     const showMeal = meals => {
-//         // const allFood = document.getElementById('all-food');
-//         // const foodName = meals.strCategory;
-//         // console.log(foodName);
-//         meals.forEach(food => {
-//             // const foodDiv = 
-//             const foodName = food.strMeal;
-//             console.log(foodName);
-//         });
-//     }
+    const showMeal = meals => {
+        const allFood = document.getElementById('all-food');
+        allFood.innerHTML = "";
+        meals.forEach(food => {
+            const foodDiv = document.createElement('div');
+            foodDiv.className = "col-2 p-2 m-2 food-box rounded-3 shadow-sm";
+            foodDiv.addEventListener("click", function () {
+                showDetails();
+            });
+            const foodImg = food.strMealThumb;
+            const foodName = food.strMeal;
+            foodDiv.innerHTML = `
+            <img width="213px" class="img-fluid rounded-3" src="${foodImg}" alt="">
 
-// })
+            <h4 class="text-center pt-2">${foodName}</h4>
+            `;
+            allFood.appendChild(foodDiv);
+
+
+        });
+    }
+    const showDetails = () => {
+        const foodDetails = document.getElementById('food-details');
+        foodDetails.className = "d-flex";
+        // const foodDetailImg = food-detail-img
+        const 
+    }
+
+})
