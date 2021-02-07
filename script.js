525401e377fdf37e4c034dbbe97718ea8e82603e
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2021-02-07 12:56:00 +0600

    api call done

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("geting start");
+const foodInput = document.getElementById('food-input');
+document.getElementById('search').addEventListener('click', function () {
+    const url = 'https://www.themealdb.com/api/json/v1/1/search.php';
+    fetch(`${url}?s=${foodInput.value}`)
+        .then(res => res.json())
+        .then(data => showMeal(data.meals))
+
+//     const showMeal = meals => {
+//         // const allFood = document.getElementById('all-food');
+//         // const foodName = meals.strCategory;
+//         // console.log(foodName);
+//         meals.forEach(food => {
+//             // const foodDiv = 
+//             const foodName = food.strMeal;
+//             console.log(foodName);
+//         });
+//     }
+
+// })
